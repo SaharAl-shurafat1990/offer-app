32ba5474876f873be34a6da554e6189d05fd94b9
MohannadBakkar <mohannadbakkar@gmail.com>
2017-03-04 19:30:55 +0200

    added model for user

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,4 +1,4 @@
-var handlers=require('../handlers.js');
+var handlers=require('./handlers.js');
 module.exports = function (app, express) {
 	app.post('/api/users/signup', handlers.handleUsers.signup);
 app.post('/api/users/signin', handlers.handleUsers.signin);
