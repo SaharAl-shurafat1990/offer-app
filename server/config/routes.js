44da6f6c719065da29275d6f37dd910a0fa7fb45
MohannadBakkar <mohannadbakkar@gmail.com>
2017-03-04 20:24:13 +0200

    signin, signup and is logedin is working now at the back-end

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,7 @@
-var handlers=require('./handlers.js');
+var userController = require('../Database/User/UserController.js');
 module.exports = function (app, express) {
-	app.post('/api/users/signup', handlers.handleUsers.signup);
-app.post('/api/users/signin', handlers.handleUsers.signin);
+app.post('/api/users/signup', userController.signup);
+app.post('/api/users/signin', userController.signin);
 	
 }
 
