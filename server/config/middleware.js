67b9867b0ba98ed50a3a54a384b0a7710b194abd
saharAl-shurafat1990 <hayelsahar@gmail.com>
2017-03-15 19:33:31 +0200

    edit bodyParser to upload big picture and edit design

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -3,6 +3,6 @@ var bodyParser = require('body-parser')
 module.exports = function (app, express) {
     app.use(morgan('dev'))
     app.use(bodyParser.urlencoded({ extended: true,limit: '50mb' }));
-    app.use(bodyParser.json())
+	app.use(bodyParser.json({limit: '50mb'}));
     app.use(express.static(__dirname + '/../../client'))
 }
