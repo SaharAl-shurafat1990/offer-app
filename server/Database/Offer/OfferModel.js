228933a3aa1649ee11cce69d7606c8f748a9c663
marwaalalawy <marwa.alalawy@gmail.com>
2017-03-22 17:43:39 +0200

    add counter

--- server/Database/Offer/OfferModel.js
+++ server/Database/Offer/OfferModel.js
@@ -6,7 +6,7 @@ var OfferSchema = new mongoose.Schema({
       type: String,
       required: true
     },
-    // location:{
+    // loc:{
     //   type:String,
     //   required: true
     // },
@@ -17,6 +17,11 @@ var OfferSchema = new mongoose.Schema({
     },
     c_id:String,
      img:String,
+
+     create_date:{
+        type: Date,
+        default: Date.now
+    },
 
     location: {
     type: [Number],
